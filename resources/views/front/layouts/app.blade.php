3edc02c0994ea0381790c1e8587d4bb0ed9abf76
mson90906250 <l90906250@gmail.com>
2020-05-23 10:51:53 +0800

    調整layout 的 footer

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -221,10 +221,6 @@
 
                         <li class="nav-item {{ Route::currentRouteName() == 'shop.index' ? 'active' : '' }}"><a class="nav-link" href="{{ route('shop.index') }}">商店</a></li>
 
-                        <li class="nav-item {{ Route::currentRouteName() == 'about' ? 'active' : '' }}"><a class="nav-link" href="about.html">關於我們</a></li>
-
-                        <li class="nav-item"><a class="nav-link" href="contact-us.html">聯絡我們</a></li>
-
                     </ul>
 
                 </div>
@@ -351,86 +347,6 @@
 
                     <div class="col-lg-4 col-md-12 col-sm-12">
 
-                        <div class="footer-top-box">
-
-                            <h3>Business Time</h3>
-
-                            <ul class="list-time">
-
-                                <li>Monday - Friday: 08.00am to 05.00pm</li>
-
-                                <li>Saturday: 10.00am to 08.00pm</li>
-
-                                <li>Sunday: <span>Closed</span></li>
-
-                            </ul>
-
-                        </div>
-
-                    </div>
-
-                    <div class="col-lg-4 col-md-12 col-sm-12">
-
-                        <div class="footer-top-box">
-
-                            <h3>Newsletter</h3>
-
-                            <form class="newsletter-box">
-
-                                <div class="form-group">
-
-                                    <input class="" type="email" name="Email" placeholder="Email Address*" />
-
-                                    <i class="fa fa-envelope"></i>
-
-                                </div>
-
-                                <button class="btn hvr-hover" type="submit">Submit</button>
-
-                            </form>
-
-                        </div>
-
-                    </div>
-
-                    <div class="col-lg-4 col-md-12 col-sm-12">
-
-                        <div class="footer-top-box">
-
-                            <h3>Social Media</h3>
-
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-
-                            <ul>
-
-                                <li><a href="#"><i class="fab fa-facebook" aria-hidden="true"></i></a></li>
-
-                                <li><a href="#"><i class="fab fa-twitter" aria-hidden="true"></i></a></li>
-
-                                <li><a href="#"><i class="fab fa-linkedin" aria-hidden="true"></i></a></li>
-
-                                <li><a href="#"><i class="fab fa-google-plus" aria-hidden="true"></i></a></li>
-
-                                <li><a href="#"><i class="fa fa-rss" aria-hidden="true"></i></a></li>
-
-                                <li><a href="#"><i class="fab fa-pinterest-p" aria-hidden="true"></i></a></li>
-
-                                <li><a href="#"><i class="fab fa-whatsapp" aria-hidden="true"></i></a></li>
-
-                            </ul>
-
-                        </div>
-
-                    </div>
-
-                </div>
-
-                <hr>
-
-                <div class="row">
-
-                    <div class="col-lg-4 col-md-12 col-sm-12">
-
                         <div class="footer-widget">
 
                             <h4>About Freshshop</h4>
@@ -438,32 +354,6 @@
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-
-                        </div>
-
-                    </div>
-
-                    <div class="col-lg-4 col-md-12 col-sm-12">
-
-                        <div class="footer-link">
-
-                            <h4>Information</h4>
-
-                            <ul>
-
-                                <li><a href="#">About Us</a></li>
-
-                                <li><a href="#">Customer Service</a></li>
-
-                                <li><a href="#">Our Sitemap</a></li>
-
-                                <li><a href="#">Terms &amp; Conditions</a></li>
-
-                                <li><a href="#">Privacy Policy</a></li>
-
-                                <li><a href="#">Delivery Information</a></li>
-
-                            </ul>
 
                         </div>
 
@@ -494,6 +384,26 @@
                                     <p><i class="fas fa-envelope"></i>Email: <a href="mailto:contactinfo@gmail.com">contactinfo@gmail.com</a></p>
 
                                 </li>
+
+                            </ul>
+
+                        </div>
+
+                    </div>
+
+                    <div class="col-lg-4 col-md-12 col-sm-12">
+
+                        <div class="footer-widget">
+
+                            <h4>Business Time</h4>
+
+                            <ul>
+
+                                <li><p>Monday - Friday: 08.00am to 05.00pm</p></li>
+
+                                <li><p>Saturday: 10.00am to 08.00pm</p></li>
+
+                                <li><p>Sunday: <span>Closed</span></p></li>
 
                             </ul>
 
